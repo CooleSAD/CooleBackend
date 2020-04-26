2c4200ec6fc3cd546bde8401c15998849a600b82
HamedKh99 <goo.hamedacc@gmail.com>
2020-04-26 19:26:47 +0430

    customized user model and added base authentication and token authentication

--- cooleBackend/urls.py
+++ cooleBackend/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls.authtoken'))
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    path('auth/', include('djoser.urls.base')),
+    path('auth/', include('djoser.urls.authtoken'))
+
 ]
