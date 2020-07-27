88a80c2541fdb56c28924f31a569e10ede6363cb
armimmo <armin9moradi@outlook.com>
2020-07-27 20:14:43 +0430

    Properties init

---
+++ properties/urls.py
@@ -0,0 +1,5 @@
+# /properties (get bezani all amval)
+# /properties/<int: pk> (post bezani, if reservable, success and reserve!)
+# /properties/me (foreign key)
+
+# mind authentican, permission class
