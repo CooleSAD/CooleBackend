bc4c0c0d6e17c68ef63ae01af518e49390935ace
armimmo <armin9moradi@outlook.com>
2020-07-28 12:45:48 +0430

    Properties Almost done, + 2 problem noted at views.py

--- properties/urls.py
+++ properties/urls.py
@@ -1,5 +1,17 @@
-# /properties (get bezani all amval)
+# /properties (get bezani all amval), if taeid nashode...
 # /properties/<int: pk> (post bezani, if reservable, success and reserve!)
 # /properties/me (foreign key)
 
-# mind authentican, permission class
+# mind authentication, Permission Class~~~
+
+
+from django.urls import path, include
+
+from .views import PropertiesListView, UserPropertiesListView, UserPropertyReserveView
+
+urlpatterns = [
+    path("properties/", PropertiesListView.as_view(), name="properties"),
+    path("properties/me/", UserPropertiesListView.as_view(), name="user_properties"),
+    path("properties/<int:pk>/", UserPropertyReserveView.as_view(), name="property_reserve")
+]
+
